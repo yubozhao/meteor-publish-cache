52d3d44ef3102d63fc9e2a130cb26e4449cd4116
Bozhao Yu <yubz86@Bozhaos-MacBook-Air.local>
2014-10-14 21:41:05 -0700

    Update package file for 0.9

--- package.js
+++ package.js
@@ -1,8 +1,13 @@
 Package.describe({
-  summary: 'Publish database cache.'
+  summary: 'Publish database cache.',
+  "version": "0.1.0",
+  "git": "https://github.com/yubozhao/meteor-publish-cache",
+  "name": "bozhao:publish-cache"
 });
 
 Package.on_use(function (api) {
+  api.versionsFrom('METEOR@0.9.0');
+
   api.use('underscore');
   api.use('jquery');
   api.add_files('publish_cache_client.js', 'client');
