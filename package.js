2873594a9d2f00e200cdfb21cb5fc06a95b93f63
Boris Kotov <boris.kotov@gmail.com>
2015-06-18 01:02:06 +0200

    Comment package.js

--- package.js
+++ package.js
@@ -1,14 +1,15 @@
 Package.describe({
   summary: 'Publish database cache.',
-  "version": "0.2.0",
+  "version": "0.2.1",
   "git": "https://github.com/yubozhao/meteor-publish-cache",
-  "name": "pub-to-cache"
+  "name": "bozhao:publish-cache"
 });
 
 Package.on_use(function (api) {
   api.versionsFrom('METEOR@0.9.0');
 
   api.use('underscore');
+  // Todo: can we get rid of jquery?
   api.use('jquery');
   api.add_files('publish_cache_client.js', 'client');
   api.add_files('publish_cache_server.js', 'server');
